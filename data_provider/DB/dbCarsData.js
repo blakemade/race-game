a433db0249ee0b862c3628b8e2f9c4e2be082200
Blake <b_jarsky@yahoo.com>
2020-08-28 14:55:43 -0700

    back from a long break

--- data_provider/DB/dbCarsData.js
+++ data_provider/DB/dbCarsData.js
@@ -1,38 +1,39 @@
-const mysql = require('mysql');
-const dotenv = require('dotenv').config();
+// const mysql = require('mysql');
+// const dotenv = require('dotenv').config();
 
-getConn = function() {
-    var conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: process.env.DB_PASSWORD,
-    database: 'race_game_test',
-    //socketPath: '/tmp/mysql.sock'
-  });
+// getConn = function() {
+//     var conn = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: process.env.DB_PASSWORD,
+//     database: 'race_game_test',
+//     //socketPath: '/tmp/mysql.sock'
+//   });
 
-  conn.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
+//   conn.connect(function(err) {
+//     if (err) {
+//       console.error('error connecting: ' + err.stack);
+//       return;
+//     }
 
-    console.log('connected as id ' + connection.threadId);
-  });
-  return conn;
-};
+//     console.log('connected as id ' + connection.threadId);
+//   });
+//   return conn;
+// };
 
 
-getManufacturers = function(onSuccess){
-  conn = getConn();
-  connection.query('SELECT * FROM manufacturers', function(err, rows, fields) {
-    if (err)
-      console.log('Error while performing Query.')
-      return
-    onSuccess(rows,fields);
+// getManufacturers = function(onSuccess){
+//   conn = getConn();
+//   connection.query('SELECT * FROM manufacturers', function(err, rows, fields) {
+//     if (err)
+//       console.log('Error while performing Query.')
+//       return
+//     onSuccess(rows,fields);
     
-});
-connection.end()
-};
+// });
+// connection.end()
+// };
 
+// didn't work, put logic in a route
 
 
--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-const mysql = require('mysql');
-const dotenv = require('dotenv').config();
+// const mysql = require('mysql');
+// const dotenv = require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: process.env.DB_PASSWORD,
-  database: 'race_game_test',
-  //socketPath: '/tmp/mysql.sock'
-})
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: process.env.DB_PASSWORD,
+//   database: 'race_game_test',
+//   //socketPath: '/tmp/mysql.sock'
+// })
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error('error connecting: ' + err.stack);
+//     return;
+//   }
 
-  console.log('connected as id ' + connection.threadId);
-});
+//   console.log('connected as id ' + connection.threadId);
+// });
 
-connection.query('SELECT * FROM manufacturers', function(err, rows, fields) {
-  if (!err)
-    console.log('The solution is: ', rows)
-  else
-    console.log('Error while performing Query.')
-})
+// connection.query('SELECT * FROM manufacturers', function(err, rows, fields) {
+//   if (!err)
+//     console.log('The solution is: ', rows)
+//   else
+//     console.log('Error while performing Query.')
+// })
 
-connection.end()
+// connection.end()
+
+// this should just import and launch app
+
+const app = require('./app.js');
+console.log('maybe launching server?')
+app.listen(3000)
